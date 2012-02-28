91cbf00b86b98f0c4510fdb537e03b12bbe35461
zhonghcc <zhonghcc@gmail.com>
2012-02-28 16:02:08 +0800

    test dymic run script

--- src/GalaEngine.js
+++ src/GalaEngine.js
@@ -1,8 +1,33 @@
-function hello(){
-	document.write("hello");
+/**
+ * @author Zhonghcc
+ */
+function loadScript(url){
+	var script = document.createElement("script");
+	script.type="text/javascript";
+	script.src=url;
+	document.body.appendChild(script);
+}
+function loadScriptString(code){
+	var script = document.createElement("script");
+	script.type="text/javascript";
+	try{
+		script.appendChild(document.createTextNode(code));
+	} catch(ex){
+		script.text=code;
+	}
+	document.body.appendChild(script);
 }
 function Init(){
+	loadScript("Libraries.js");
 	ConfigInit();
+	GalaStart();
+}
+function GalaStart(){
+	var node=document.createElement("input");
+	node.type="button";
+	node.value="click!";
+	node.onclick=function(){hello();};
+	document.body.appendChild(node);
 }
 function ConfigInit(){
 	
---
+++ src/Libraries.js
@@ -0,0 +1,30 @@
+/**
+ * @author Zhonghcc
+ */
+function hello(){
+	document.write("hello");
+}
+var EventUtil={
+	addHandler:function(element,type,handler){
+		if(element.addEventListener){
+			element.addEventListener(type,handler,false);
+		}
+		else if(element.attachEvent){
+			element.attachEvent("on"+type,handler);
+		}
+		else {
+			element["on"+type]=handler;
+		}
+	},
+	removeHandler:function(element,type,handler){
+		if(element.removeEventListener){
+			element.removeEventListener(type,handler,false);
+		}
+		else if(element.attachEvent){
+			element.detachEvent("on"+type,handler);
+		}
+		else {
+			element["on"+type]=null;
+		}
+	}
+}
